b24e7118495b4195902c5cea2cd15b5aa1d1aa76
Pablo <115142181+Readypablo@users.noreply.github.com>
2023-03-23 20:04:07 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
 
 <div class="header-block">
 
-<p> 8 (800) 111-11-11 </p>
 
 </div>
 </header>
@@ -35,7 +34,9 @@
 
 
 <main>
+<div class="interval">
 
+</div>
 <div class="section-info-red">
     <div class="txt-info-red">
         <p> <span> Инновационный сервис <br> Бронирования</span></p>
@@ -51,32 +52,135 @@
     </div>
 </div>
 
-
+<div class="interval">
+    
+</div>
 
 
 <div class="section-search">
 
+<div class="disp-flex">
     <div class="img-slide">
-
+        <img src="../img/rav4.png" alt="" id="one-rav">
     </div>
+    <div> <p>Выбери сам <br> Свой<br> Автомобиль! </p> </div>
+</div>
+
 
     <div class="form-search">
+        <div>
+        <input type="text" class="inp-type" placeholder="Марка"><input type="text" class="inp-type" placeholder="Модель">
+        <div>
+            <input type="radio" value="" class="chec-type"  >Эконом</input>
+            <input type="radio" value="" class="chec-type" >Премиум </input>
+            <input type="button" value="Найти автомобиль" class="btn-type">
+        </div>
+        </div>
 
+      
     </div>
 
 </div>
 
+<div class="interval">
+
+</div>
+<!-- 1 -->
+<div class="news-block">
+        <div class="img-news">
+        <img src="../img/news1.jpg" alt=""  height="500px">
+        </div>
+
+        <div class="txt-news">
+            <p><span> В России вынесли первый приговор за продажу аккаунтов каршеринга</span><br><br>
+            В конце 2020 года в России вынесли первый приговор за продажу аккаунтов каршеринга. Приморский районный суд Петербурга оштрафовал гражданина РФ Романа Амелина на 50 тыс. рублей.
+
+        Как сообщает РАПСИ со ссылкой на Объединенную пресс-службу судов Петербурга, 21-летний молодой человек обвинялся по части 2 статьи 272 УК РФ.
+
+            </p>
+            <input type="button" value="смотреть новость" class="btn-news">
+        </div>
+</div>
+
+<div class="interval">
+</div>
+<!-- 2 -->
+<div class="news-block">
+    <div class="txt-news">
+                <p><span> Программа поддержки каршеринга не заработала из-за российского телематического оборудования</span><br><br>
+               25 августа 2020 года стало известно о том, что программа поддержки каршеринга, которая была анонсирована ещё в апреле на совещании у президента РФ Владимира Путина, так и не заработала.
+
+                </p>
+                <input type="button" value="смотреть новость" class="btn-news">
+            </div>
+            <div class="img-news">
+            <img src="../img/news2.jpg" alt=""  height="500px">
+            </div>
+</div>
 
 
+
+<div class="interval">
+</div>
+<!-- 3 -->
+<div class="news-block">
+        <div class="img-news">
+        <img src="../img/news3.jpg" alt=""  height="500px">
+        </div>
+
+        <div class="txt-news">
+            <p><span> Мошенничество с данными клиентов</span><br><br>
+            По сообщениям на июль 2020 г каршеринг становится отличным помощником для мошенников. С помощью селфи клиентов с паспортом на людей берут кредиты в микрофинансовых компаниях.
+
+Также базы каршерингов используют для звонков из фальшивой службы безопасности банков - так можно узнать наличие денег на счёте, данные банковских карт.
+            </p>
+            <input type="button" value="смотреть новость" class="btn-news">
+        </div>
+</div>
+
+<div class="interval">
+</div>
+<!-- 4 -->
+<div class="news-block">
+    <div class="txt-news">
+                <p><span> Столичный каршеринг обвинил Роспотребнадзор в блокировке деятельности. Новые правила операторам объяснять не спешат</span><br><br>
+                Каршеринг не может возобновить работу по докоронавирусному сценарию из-за отсутствия предписаний Роспотребнадзора. Представители сервисов обвинили ведомство в блокировке их деятельности и 9 июня направили коллективное обращение.
+                </p>
+                <input type="button" value="смотреть новость" class="btn-news">
+            </div>
+            <div class="img-news">
+            <img src="../img/news4.jpg" alt=""  height="500px">
+            </div>
+</div>
 
 </main>
 
-
+<div class="interval">
+</div>
 
 
 
 <footer>
 
+<div class="footer-block">
+<p> <span>Индекс Драйв </span><br>
+сервис каршеринга в россии
+</p>
+</div>
+
+<div class="footer-block">
+    
+<a href="#" class="link-header"><p> Акции </p></a>
+<a href="#" class="link-header"><p> Бронирование </p></a>
+<a href="#" class="link-header"><p> Личный кабинет </p></a>
+
+</div>
+
+<div class="footer-block">
+
+<p> 8 (800) 111-11-11 </p>
+
+</div>
 </footer>
 
 
